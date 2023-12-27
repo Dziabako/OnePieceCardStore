910834023c4f48729de473e777d2722ba8d00443
Dziabak <dziabanux@gmail.com>
2023-12-27 21:45:07 +0100

    Added add_card file

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, IntegerField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config.from_pyfile("config.cfg")
+db = SQLAlchemy(app)
+app.app_context().push()
 
 
 @app.route("/")
