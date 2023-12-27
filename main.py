aa735a238d5ac70bccc5ea2f0114d55f04cee696
Dziabak <dziabanux@gmail.com>
2023-12-27 22:16:07 +0100

    Create FlaskSQL database

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, IntegerField
+from wtforms.fields import StringField, IntegerField, FloatField
 from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 app.config.from_pyfile("config.cfg")
 db = SQLAlchemy(app)
 app.app_context().push()
 
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    version = db.Column(db.String(10))
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    image = db.Column(db.String(250))
+
+
+class CardForm(FlaskForm):
+    name = StringField("Name")
+    version = StringField("Version")
+    quantity = IntegerField("Quantity")
+    price = FloatField("Price")
+    image = StringField()
+
+db.create_all()
 
 @app.route("/")
 def index():
